8efc0a877e9dbd0d1eff937f87434740a22a5631
Leah Achieng <olaachieng123@gmail.com>
2026-05-22 15:41:49 +0300

    fix cors for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,10 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  origin: [
+    'http://localhost:4200',
+    'https://roommate-compatibility-system.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
 
   await app.listen(process.env.PORT ?? 3000);
 }
