921f280c4107dd03385952a2582c47a580ae3928
Leah Achieng <olaachieng123@gmail.com>
2026-05-22 14:39:09 +0300

    finally delete prisma config

--- backend/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  earlyAccess: true,
-  schema: 'prisma/schema.prisma',
-});
